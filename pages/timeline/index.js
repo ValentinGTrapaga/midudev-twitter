3e13dde398c1b54af60090c3fe1b655959d01aa3
ValentinGTrapaga <gonzaleztrapagav@gmail.com>
2022-09-01 08:59:46 -0300

    First commit

---
+++ pages/timeline/index.js
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import AppLayout from '../../components/AppLayout'
+import styles from '../../styles/Home.module.css'
+
+export default function Timeline({ userName }) {
+  return (
+    <div className={styles.container}>
+      <AppLayout>
+        <h1>This is the timeline of {userName}</h1>
+        <Link href='/'>
+          <a>Go home</a>
+        </Link>
+      </AppLayout>
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:3000/api/hello')
+  const data = await res.json()
+  const { userName } = data
+  return {
+    props: {
+      userName
+    }
+  }
+}
